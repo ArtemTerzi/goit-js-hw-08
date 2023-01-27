f94be9dde038745ac7e1819b3fc595da9e0e0701
Artem Terzi <terzi.artem.yuriyovych@gmail.com>
2023-01-27 17:39:54 +0200

    1st task done(added simplelightbox lib) + changed package.json config

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
-// Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+
+function galleryMarkupCreator(array) {
+  return array
+    .map(
+      ({ original, preview, description }) => `
+    <a class="gallery__item" href=${original}>
+      <img
+        class ="gallery__image"
+        src=${preview}
+        alt=${description}
+      />
+    </a>`
+    )
+    .join('');
+}
+
+galleryRef.innerHTML = galleryMarkupCreator(galleryItems);
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
