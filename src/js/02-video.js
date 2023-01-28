7e69622476fd0328c1a02beeff0806eff5fb6728
Artem Terzi <terzi.artem.yuriyovych@gmail.com>
2023-01-28 19:02:47 +0200

    second task done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const vimeoPlayer = document.querySelector('#vimeo-player');
+const player = new Player(vimeoPlayer);
+
+player.on(
+  'timeupdate',
+  throttle(value => {
+    localStorage.setItem('videoplayer-current-time', JSON.stringify(value));
+  }, 1000)
+);
+
+const curTime = JSON.parse(localStorage.getItem('videoplayer-current-time'));
+if (curTime !== null) player.setCurrentTime(curTime.seconds);
